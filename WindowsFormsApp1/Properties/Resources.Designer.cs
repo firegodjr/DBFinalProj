a61292836f17f034a54a54add8a24806c57f92dc
Dan McConnell <dvm8g5@mst.edu>
2020-05-06 11:34:36 -0500

    Added Insert_Script to Resources

--- WindowsFormsApp1/Properties/Resources.Designer.cs
+++ WindowsFormsApp1/Properties/Resources.Designer.cs
@@ -61,31 +61,45 @@ namespace DBFinalProj.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to CREATE TABLE ROOM
+        ///   Looks up a localized string similar to CREATE TABLE RESIDENT
+        ///(	SSN					INT				NOT NULL,
+        ///	PhoneN				VARCHAR(20),
+        ///	Name				VARCHAR(30)		NOT NULL,
+        ///	OutstandingBalance	INT,
+        ///	MissedChores		TINYINT,
+        ///	SmallGroup			VARCHAR(50),	
+        ///	NextSemesterPlan	VARCHAR(50),
+        ///	GradDate			DATE,
+        ///	ContractStartDate	DATE			NOT NULL,
+        ///	ContractEndDate		DATE			NOT NULL,
+        ///	ContractMealPlan	VARCHAR(20),
+        ///	KP					BOOL			NOT NULL,
+        ///	FM					BOOL			NOT NULL,
+        ///	Primary Key(SSN)
+        ///);
+        ///
+        ///CREATE TABLE ROOM
         ///(	RoomNumber		SMALLINT		NOT NULL,
         ///	ChoreSet		VARCHAR(10)		NOT NULL,
-        ///	WorkOrders		VARCHAR(100),
-        ///	RoomMeetingTime	VARCHAR(30)		NOT NULL,
-        ///	RLSSN			INT				NOT NULL,
-        ///	PRIMARY KEY(RoomNumber),
-        ///	FOREIGN KEY(RLSSN) REFERENCES RESIDENT(SSN)
-        ///);
-        ///
-        ///CREATE TABLE HOUSE_SECTION
-        ///(	Name		VARCHAR(30)		NOT NULL,
-        ///	Points		SMALLINT,
-        ///	RASSN		INT				NOT NULL,
-        ///	FOREIGN KEY(RASSN) REFERENCES RESIDENT(SSN)
-        ///	PRIMARY KEY(Name)
-        ///);
-        ///
-        ///CREATE TABLE RESIDENT
-        ///(	SSN					INT				NOT NULL,
-        ///	PhoneN				VARCHAR( [rest of string was truncated]&quot;;.
+        ///	 [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string Create_Script {
             get {
                 return ResourceManager.GetString("Create_Script", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to INSERT INTO resident
+        ///VALUES 
+        ///(000000001, &quot;618-618-6118&quot;, &quot;Kermit Kermidian&quot;, 250, 6, &quot;Kermit&apos;s Deep Talks&quot;, &quot;Kermit Plans to Move Out&quot;, 20220522, 20170818,20220517,&quot;Meal Plan 1&quot;,1, 0),
+        ///(000000002, &quot;666-654-7890&quot;, &quot;Ms. Piggy&quot;, 6, 0, &quot;Kermit&apos;s Deep Talks&quot;, &quot;Co-Op in Nashville&quot;, 20220522, 20170818,20220517,&quot;Meal Plan 3&quot;,0, 0),
+        ///(000000003, &quot;666-654-7891&quot;, &quot;Grover&quot;, 1200, 13, &quot;Kermit&apos;s Deep Talks&quot;, &quot;Stay in House&quot;, 20220522, 20170818,20220517,&quot;Meal Plan 2&quot;,0, 0),
+        ///(000000004, &quot;666-654-2222&quot;, &quot;Cookie Monster&quot; [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string Insert_Script {
+            get {
+                return ResourceManager.GetString("Insert_Script", resourceCulture);
             }
         }
     }
